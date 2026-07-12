e0b24e020a930cf0f03ac263c9e3e25ba66689e9
RonnieK2016 <ronnieo@gmail.com>
2026-07-12 15:58:07 -0400

    Added leader election algorithm

--- src/main/java/com/udemy/distrsystems/LeaderElection.java
+++ src/main/java/com/udemy/distrsystems/LeaderElection.java
@@ -1,20 +1,21 @@
 package com.udemy.distrsystems;
 
 import lombok.extern.slf4j.Slf4j;
-import org.apache.zookeeper.KeeperException;
-import org.apache.zookeeper.WatchedEvent;
-import org.apache.zookeeper.Watcher;
-import org.apache.zookeeper.ZooKeeper;
+import org.apache.zookeeper.*;
 
 import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
 
 @Slf4j
 public class LeaderElection implements Watcher {
 
     public static final String ZOOKEEPER_ADDRESS = "127.0.0.1:2181";
-    public static final int ZOOKEPER_SESSION_TIMEOUT = 3000;
+    public static final int ZOOKEEPER_SESSION_TIMEOUT = 3000;
+    public static final String ELECTION_NAMESPACE = "/election";
 
     private ZooKeeper zk;
+    private String zNodeIdx;
 
     public static void main(String[] args) throws IOException, InterruptedException {
         LeaderElection leaderElection = new LeaderElection();
@@ -24,8 +25,27 @@ public class LeaderElection implements Watcher {
         log.info("Exiting LeaderElection");
     }
 
+    public void volunteerForLeadership() throws InterruptedException, KeeperException {
+        String zNodePrefix = ELECTION_NAMESPACE + "/c_";
+        String zNodeFullPath = zk.create(zNodePrefix, new byte[]{}, ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL_SEQUENTIAL);
+        log.info("Created znode {}.", zNodeFullPath);
+        this.zNodeIdx = zNodeFullPath.replace(ELECTION_NAMESPACE + "/", "");
+    }
+
+    public void electLeader() throws InterruptedException, KeeperException {
+        List<String> children = zk.getChildren(ELECTION_NAMESPACE, false);
+        Collections.sort(children);
+        String smallestChild = children.get(0);
+        if(smallestChild.equals(zNodeIdx)) {
+            log.info("Current node is the leader");
+            return;
+        }
+
+        log.info("Current node is not the leader. Leader is {}", smallestChild);
+    }
+
     public void connectZookeeper() throws IOException {
-        this.zk = new ZooKeeper(ZOOKEEPER_ADDRESS, ZOOKEPER_SESSION_TIMEOUT, this);
+        this.zk = new ZooKeeper(ZOOKEEPER_ADDRESS, ZOOKEEPER_SESSION_TIMEOUT, this);
     }
 
     public void run() throws InterruptedException {
