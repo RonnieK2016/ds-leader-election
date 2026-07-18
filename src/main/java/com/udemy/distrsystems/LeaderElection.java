2d579a70bfca0812f1b39be8d619475fc535f116
RonnieK2016 <ronnieo@gmail.com>
2026-07-18 15:37:47 -0400

    implement different watch events

--- src/main/java/com/udemy/distrsystems/LeaderElection.java
+++ src/main/java/com/udemy/distrsystems/LeaderElection.java
@@ -2,6 +2,7 @@ package com.udemy.distrsystems;
 
 import lombok.extern.slf4j.Slf4j;
 import org.apache.zookeeper.*;
+import org.apache.zookeeper.data.Stat;
 
 import java.io.IOException;
 import java.util.Collections;
@@ -13,13 +14,16 @@ public class LeaderElection implements Watcher {
     public static final String ZOOKEEPER_ADDRESS = "127.0.0.1:2181";
     public static final int ZOOKEEPER_SESSION_TIMEOUT = 3000;
     public static final String ELECTION_NAMESPACE = "/election";
+    private String electedLeader;
 
     private ZooKeeper zk;
     private String zNodeIdx;
 
-    public static void main(String[] args) throws IOException, InterruptedException {
+    public static void main(String[] args) throws IOException, InterruptedException, KeeperException {
         LeaderElection leaderElection = new LeaderElection();
         leaderElection.connectZookeeper();
+        leaderElection.volunteerForLeadership();
+        leaderElection.electLeader();
         leaderElection.run();
         leaderElection.close();
         log.info("Exiting LeaderElection");
@@ -36,12 +40,19 @@ public class LeaderElection implements Watcher {
         List<String> children = zk.getChildren(ELECTION_NAMESPACE, false);
         Collections.sort(children);
         String smallestChild = children.get(0);
-        if(smallestChild.equals(zNodeIdx)) {
+        if (smallestChild.equals(zNodeIdx)) {
             log.info("Current node is the leader");
             return;
         }
 
+        this.electedLeader = smallestChild;
+        watchLeader();
         log.info("Current node is not the leader. Leader is {}", smallestChild);
+    }
+
+    public void watchLeader() throws InterruptedException, KeeperException {
+        Stat leaderNodeExists = zk.exists(ELECTION_NAMESPACE + "/" + this.electedLeader, this);
+        log.info("Received stat on leader node {}.", leaderNodeExists);
     }
 
     public void connectZookeeper() throws IOException {
@@ -65,13 +76,24 @@ public class LeaderElection implements Watcher {
             case None:
                 if (event.getState() == Watcher.Event.KeeperState.SyncConnected) {
                     log.info("Zookeeper Connected");
-                }
-                else {
+                } else {
                     synchronized (zk) {
                         log.info("Zookeeper Disconnected");
                         zk.notifyAll();
                     }
                 }
+                break;
+            case NodeDataChanged:
+                log.info("Zookeeper node data changed event - {}", event);
+                break;
+            case NodeChildrenChanged:
+                log.info("Zookeeper node children changed - {}", event);
+                break;
+            case NodeCreated:
+                log.info("Zookeeper node created - {}", event);
+                break;
+            case NodeDeleted:
+                log.info("Zookeeper node deleted - {}", event);
         }
     }
 }
